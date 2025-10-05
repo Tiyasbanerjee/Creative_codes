dfc7341012f49877e67b2b8bd94cfdad98790ae9
Tiyas Banerjee <181446532+Tiyasbanerjee@users.noreply.github.com>
2025-10-05 10:56:17 +0530

    Validate bounce back percentage input
    Added validation for bounce back percentage input.

--- Finding_the_total_displacement_of_a_ball_felt_from_a_certain_distance_and_bounces_back/python code/main.py
+++ Finding_the_total_displacement_of_a_ball_felt_from_a_certain_distance_and_bounces_back/python code/main.py
@@ -2,7 +2,11 @@ while True:
     try:
             x:int = int(input("Enter the distance from which the ball is felt:--->> "))
             a:int = int(input("Enter the percentage the ball bounces back to the privious hight:--->> "))
-            break
+            if a <= 0 or a >= 100 :
+                 print("a ball cant bounce back more than the hight it felt, and if happen and this program will not support that case.")
+                 continue
+            else:
+                  break
     except ValueError:
             print("Please enter a valid integer")
             continue
